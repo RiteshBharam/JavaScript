f4821fb956a302b614d9a50c07d8b9e781595adc
RiteshBharam <73223683+RiteshBharam@users.noreply.github.com>
2023-09-29 21:18:51 +0530

    Add files via upload

---
+++ JavaScript/Excercises/3.js
@@ -0,0 +1,34 @@
+//3a
+console.log('My name is : '+'Ritesh')
+
+//3b
+console.log('Ritesh')
+
+//3c
+console.log('Ritesh '+' Bharam')
+
+//3d
+console.log('Total cost : $'+(5+3))
+
+//3e
+alert('Total cost : $ '+(5+3))
+
+//3f,3g
+console.log('Total cost :'+(599+295)/100);
+
+//3h
+console.log(`Total cost: $`+(599+295)/100);
+
+//3i
+alert(`Total cost: $`+(599+295)/100);
+
+//3j
+alert(`Total cost: $ `+(599+295)/100+`
+Thank You,come again !`);
+
+//3k
+alert(`Items(4) : `+'$'+57.88+`
+Shipping & handling : `+`$`+9.98+`
+Total before tax : `+'$'+((5788+998)/100)+`
+Estimated tax(10%) : `+'$'+6.79+`
+Order Total : `+'$'+((5788+998+679)/100));
